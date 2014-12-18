aaab5a876ef51e9f2e6768109acbb0b40439147a
pikeBishop <syntaxError@konto.pl>
2014-12-18 22:13:09 +0100

    finished version

--- examples/geoSections/geoSectionGpxSummary.py
+++ examples/geoSections/geoSectionGpxSummary.py
@@ -3,10 +3,12 @@ from examples import gpxpy
 import math as mod_math
 
 
-class geoSectionGpxSummaryCommon():
+class geoSectionGpxSummaryCommon(dict):
     gpxSegment = None
+    indentation = "  "
 
-    def __init__(self):
+    def __init__(self, *arg,**kw):
+        super(geoSectionGpxSummaryCommon, self).__init__(*arg, **kw)
         pass
 
     def processGpx(self):
@@ -41,24 +43,25 @@ class geoSectionGpxSummaryCommon():
             previous_point = point
         self["Avg distance between points"]=sum(distances) / len(list(self.gpxSegment.walk()))
 
-    def printGpxSegmentInfo(self, indentation='    '):
-        print('%sLength 2D: %s' % (indentation, self["Length 2D"]))
-        print('%sLength 3D: %s' % (indentation, self["Length 3D"]))
+    def __str__(self):
+        string = ""
 
-        print('%sMoving time: %s' % (indentation, self["Moving time"]))
-        print('%sStopped time: %s' % (indentation, self["Stopped time"]))
-#        print('%sStopped distance: %sm' % (indentation, self["Stopped distance"]))
-        print('%sMax speed: %sm/s = %skm/h' % (indentation, self["Max speed"], self["Max speed"] * 60. ** 2 / 1000. if self["Max speed"] else 0))
+        string += "{0}{1:<20s}{2}{3:.2f} km\n\r".format(self.indentation, "Length 2D:", "", self["Length 2D"])
+        string += "{0}{1:<20s}{2}{3:.2f} km\n\r".format(self.indentation, "Length 3D:", "", self["Length 3D"])
 
-        print('%sTotal uphill: %sm' % (indentation, self["Total uphill"]))
-        print('%sTotal downhill: %sm' % (indentation, self["Total downhill"]))
+        string += "{0}{1:<20s}{2}{3}\n\r".format(self.indentation, "Moving time:", "", self["Moving time"])
+        string += "{0}{1:<20s}{2}{3}\n\r".format(self.indentation, "Stopped time:", "", self["Stopped time"])
+        string += "{0}{1:<20s}{2}{3:.2f} m/s = {4:.2f} km/h\n\r".format(self.indentation, "Max speed:", "", self["Max speed"], self["Max speed"] * 60. ** 2 / 1000. if self["Max speed"] else 0)
 
-        print('%sStarted: %s' % (indentation, self["Started"]))
-        print('%sEnded: %s' % (indentation, self["Ended"]))
+        string += "{0}{1:<20s}{2}{3:.0f} m\n\r".format(self.indentation, "Total uphill:", "", self["Total uphill"])
+        string += "{0}{1:<20s}{2}{3:.0f} m\n\r".format(self.indentation, "Total downhill:", "", self["Total downhill"])
 
-        print('%sAvg distance between points: %sm' % (indentation, self["Avg distance between points"]))
+        string += "{0}{1:<20s}{2}{3}\n\r".format(self.indentation, "Started:", "", self["Started"])
+        string += "{0}{1:<20s}{2}{3}\n\r".format(self.indentation, "Ended:", "", self["Ended"])
 
-        print('')
+#        string += "{0}{1:<20s}{2}{3:.2f} m\n\r".format(self.indentation, "Avg distance between points:", "", self["Avg distance between points"])
+
+        return string
 
     def formatTime(self, time_s):
         if not time_s:
@@ -68,39 +71,10 @@ class geoSectionGpxSummaryCommon():
 
         return '%s:%s:%s' % (str(int(hours)).zfill(2), str(int(minutes % 60)).zfill(2), str(int(time_s % 60)).zfill(2))
 
+class geoSectionGpxSegmentSummary(geoSectionGpxSummaryCommon):
 
-class geoSectionGpxFileSummary(geoSectionGpxSummaryCommon, dict):
-    gpxPy = None
-
-    def __init__(self, gpxFileName=None, *arg,**kw):
-        geoSectionGpxSummaryCommon.__init__(self)
-        super(geoSectionGpxFileSummary, self).__init__(*arg, **kw)
-
-        try:
-            gpxFile = open(gpxFileName, 'r')
-            self.gpxSegment = gpxpy.parse(gpxFile)
-
-            self.processGpx()
-
-            for track_no, track in enumerate(self.gpxSegment.tracks):
-                for segment_no, segment in enumerate(track.segments):
-                    self["Segments"]=[]
-                    geoSectionGpxSegmentSummary(gpxSegment=segment, parentGpxSummary=self)
-
-        except Exception as e:
-            print("Error opening or parsing {0}: {1}.".format(gpxFileName, e))
-
-    def printGpxSegmentInfo(self, indentation='    '):
-        geoSectionGpxSummaryCommon.printGpxSegmentInfo(self)
-
-        for gpxSegmentSummary in self["Segments"]:
-            gpxSegmentSummary.printGpxSegmentInfo(indentation="               ")
-
-class geoSectionGpxSegmentSummary(geoSectionGpxSummaryCommon, dict):
-
-    def __init__(self, gpxSegment=None, parentGpxSummary=None, *arg,**kw):
-        geoSectionGpxSummaryCommon.__init__(self)
-        super(geoSectionGpxSegmentSummary, self).__init__(*arg, **kw)
+    def __init__(self, gpxSegment, parentGpxSummary, *arg, **kw):
+        geoSectionGpxSummaryCommon.__init__(self, *arg, **kw)
 
         self.gpxSegment = gpxSegment
 
@@ -112,46 +86,46 @@ class geoSectionGpxSegmentSummary(geoSectionGpxSummaryCommon, dict):
         except Exception as e:
             print("Error parsing gpxSegment: {0}.".format(e))
 
+class geoSectionGpxDataSummary(geoSectionGpxSummaryCommon):
+
+    def __init__(self, gpxData, *arg, **kw):
+#        super(geoSectionGpxDataSummary, self).__init__(*arg, **kw)
+
+        try:
+            self.gpxSegment = gpxData
+
+            self.processGpx()
+
+            for track_no, track in enumerate(self.gpxSegment.tracks):
+                for segment_no, segment in enumerate(track.segments):
+                    self["Segments"]=[]
+                    geoSectionGpxSegmentSummary(gpxSegment=segment, parentGpxSummary=self)
+
+        except Exception as e:
+            print("Error opening or parsing: {0}.".format(e))
+
+    def __str__(self):
+        str = ""
+
+        str += geoSectionGpxSummaryCommon.__str__(self)
+
+        for gpxSegmentSummary in self["Segments"]:
+            gpxSegmentSummary.indentation = gpxSegmentSummary.indentation + gpxSegmentSummary.indentation
+            str += "\n\r"
+            str += gpxSegmentSummary.__str__()
+
+        return str
+
+class geoSectionGpxFileSummary(geoSectionGpxDataSummary):
+    gpxPy = None
+
+    def __init__(self, gpxFileName=None):
+        try:
+            gpxFile = open(gpxFileName, 'r')
+            geoSectionGpxDataSummary.__init__(self, gpxpy.parse(gpxFile))
+        except Exception as e:
+            print("Error opening or parsing {0}: {1}.".format(gpxFileName, e))
 
 if __name__ == "__main__":
     geoSectionGpxSummary = geoSectionGpxFileSummary(gpxFileName = '../aa.gpx')
-    geoSectionGpxSummary.printGpxSegmentInfo()
-
-
-# from examples import gpxpy
-# from geoNamesAdapter import *
-# from rtree import index
-#
-# class geoNamesCommon():
-#     def __init__(self):
-#         pass
-#
-# class geoNames(geoNamesCommon, list):
-#     def __init__(self, geoPoints=None, geoNamesAdapter=None, *arg,**kw):
-#         geoNamesCommon.__init__(self)
-#         super(geoNames, self).__init__(*arg, **kw)
-#
-#         if(geoPoints!=None and geoNamesAdapter!=None):
-#             rtreeIdx = index.Index()
-#             for i, geoName in enumerate(geoNamesAdapter):
-#                 print("{0}: {1}".format(i, geoName))
-#                 rtreeIdx.insert(i, (geoName["longitude"], geoName["latitude"], geoName["longitude"], geoName["latitude"]), obj=geoName)
-#
-#             for i, geoPoint in enumerate(geoPoints):
-#                 print("-{0}- {1}, {2}:".format(i, geoPoint.latitude, geoPoint.longitude))
-#                 obj = list(rtreeIdx.nearest((geoPoint.longitude, geoPoint.latitude, geoPoint.longitude, geoPoint.latitude), objects=True))[0].object
-#                 if obj not in self:
-#                     self.append(obj)
-#
-# if __name__ == "__main__":
-#
-#     _geoNamesAdapterCsvFile_geoNames = geoNamesAdapterCsvFile_geoNames("./PL.txt")
-#     _geoNamesAdapterCsvFile_geoPortal = geoNamesAdapterCsvFile_geoPortal("./obiekty_fizjograficzne.csv")
-#
-#     gpx_file = open('../aa.gpx', 'r')
-#     gpx = gpxpy.parse(gpx_file)
-#     points = gpx.tracks[0].segments[0].points
-#
-#     geoNamesGpx = geoNames(points, _geoNamesAdapterCsvFile_geoNames)
-#     for i, geoNameGpx in enumerate(geoNamesGpx):
-#         print(geoNameGpx)
+    print(geoSectionGpxSummary)
